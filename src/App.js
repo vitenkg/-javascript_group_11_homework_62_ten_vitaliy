ab86b3eedc0e12937299b9d29768ecd55a9b159b
Vitaliy Ten <vitalten10@gmail.com>
2021-08-22 17:24:16 +0600

    Установлен react-creact-dom создана навигация указаны некоторые стили кнопок

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./Component/Counteiner/Home/Home";
+import AboutUs from "./Component/Counteiner/AboutUs/AboutUs";
+import Contact from "./Component/Counteiner/Contact/Contact";
+import {BrowserRouter, NavLink, Route, Switch} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+    <div className="Container">
+        <div>
+            <BrowserRouter>
+                <ul>
+                    <li><NavLink exact className="Link" to="/">Home</NavLink></li>
+                    <li><NavLink className="Link" to="/aboutus">About US</NavLink></li>
+                    <li><NavLink className="Link" to="/contact">Contact</NavLink></li>
+
+                </ul>
+
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/aboutus" component={AboutUs}/>
+                    <Route path="/contact" component={Contact}/>
+                </Switch>
+        </BrowserRouter>
+        </div>
     </div>
-  );
-}
+
+)
+
 
 export default App;
---
+++ src/Component/Counteiner/AboutUs/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AboutUs.css';
+
+const AboutUs = () => {
+    return (
+        <div>
+            About US
+        </div>
+    );
+};
+
+export default AboutUs;
